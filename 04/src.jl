9859c4a0c294ae3f290f2ed5b9ced48b963a6743
Moritz Klein <moritz.klein@pm.me>
2021-12-04 11:24:57 +0100

    Clean solution 04 a bit

--- 04/src.jl
+++ 04/src.jl
@@ -16,8 +16,8 @@ boards = parse.(Int, boards)
 
 
 # sum all unmarked numbers and multiply by last draw
-function caluclate_score(boards, checked, draw, idx)
-    c = checked[:,:,idx] .|> Bool .|> ~
+function caluclate_score(boards, marked, draw, idx)
+    c = marked[:,:,idx] .|> Bool .|> ~
     b = boards[:,:,idx]
 
     sum(b[c]) * draw
@@ -25,29 +25,34 @@ end
 
 
 # check if either in a row or column 5 numbers are marked
-function winner_idx(checked)
-    rows = sum(checked, dims=1)
-    cols = sum(checked, dims=2)
+function winner_idx(marked)
+    rows = sum(marked, dims=1)
+    cols = sum(marked, dims=2)
     cols = permutedims(cols, (2,1,3))
+
     findall(x -> x == 5, [cols; rows])
+end
+
+
+# mark draw on checkerboard (set to 1/true)
+function mark_draw!(marked, draw)
+    idx = findall(x -> x == draw, boards)
+    marked[idx] .= 1
+
+    return
 end
 
 
 # part 1
 begin
-    checked = zeros(Int, size(boards))
+    marked = zeros(Int, size(boards))
 
     for draw ∈ draws
-        # mark draw
-        idx = findall(x -> x == draw, boards)
-        checked[idx] .= 1
+        mark_draw!(marked, draw)
+        win_idx = winner_idx(marked)
 
-        # check for completed rows/cols
-        win_idx = winner_idx(checked)
-
-        # someone won!
         if length(win_idx) > 0
-            @show caluclate_score(boards, checked, draw, win_idx[1][3])
+            @show caluclate_score(boards, marked, draw, win_idx[1][3])
             break
         end
     end
@@ -56,25 +61,19 @@ end
 
 # part 2
 begin
-    checked = zeros(Int, size(boards))
+    marked = zeros(Int, size(boards))
 
     for draw ∈ draws
-        global boards, checked
+        global boards, marked
 
-        # mark draw
-        idx = findall(x -> x == draw, boards)
-        checked[idx] .= 1
+        mark_draw!(marked, draw)
+        win_idx = winner_idx(marked)
 
-        # check for completed rows/cols
-        win_idx = winner_idx(checked)
-
-        # remove completed players / winners
         if length(win_idx) > 0
             players = 1:size(boards, 3)
 
-            # last winning player standing wins
             if length(players) == 1
-                @show caluclate_score(boards, checked, draw, 1)
+                @show caluclate_score(boards, marked, draw, 1)
                 break
             end
 
@@ -82,7 +81,7 @@ begin
             winners = [w[3] for w ∈ win_idx]
             players = setdiff(players, winners)
             boards = boards[:,:,players]
-            checked = checked[:,:,players]
+            marked = marked[:,:,players]
         end
     end
 end
